faaa70be718d20d27490191553a46226957fbec3
Aldrei Glenn Nuqui <68434908+SuichiNagi@users.noreply.github.com>
2021-06-03 14:38:12 +0800

    Add files via upload

---
+++ Other jQuery Libraries/script.js
@@ -0,0 +1,23 @@
+$(document).ready(function(){
+    $('#page, #page2, #page3').accordion({
+        heightStyle: "fill",
+        animate: 500,
+        collapsible: true
+    });
+
+    $( "#tabs" ).tabs({
+        collapsible: true,        
+    });
+
+    $('#ui-id-19, #ui-id-20, #ui-id-21').click(function(){
+        $('body').css('background', 'url(type-r/TYPE-R-EXTERIOR-1.jpg)');
+        $('h1').css('color', 'white');
+        $('h1').text('HONDA CIVIC TYPE R')
+    });
+
+    (function($){
+
+        $('.swipebox').swipebox();
+    
+    })(jQuery);
+});
